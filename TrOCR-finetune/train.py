59d081aa27f319de75fe70423cb41796efa01021
Mohammed20201991 <mohamedabid092@gmail.com>
2022-10-27 15:56:02 +0200

    ViT with gpt2-small trained on Hungarian Poem

--- TrOCR-finetune/train.py
+++ TrOCR-finetune/train.py
@@ -1,8 +1,10 @@
 import fun
 import unit_test
-fun.os.environ['CUDA_VISIBLE_DEVICES'] =  '5' 
+fun.os.environ['CUDA_VISIBLE_DEVICES'] = '6'
+
 # from transformers import AutoTokenizer
-fun.processor.tokenizer = fun.AutoTokenizer.from_pretrained("SZTAKI-HLT/hubert-base-cc")
+fun.processor.tokenizer = fun.AutoTokenizer.from_pretrained("NYTK/text-generation-poem-petofi-gpt2-small-hungarian")
+
 def main():
     df = fun.load_laia()
     # df = load_dataset()
@@ -23,20 +25,21 @@ def main():
     label_str = fun.processor.decode(labels, skip_special_tokens=True)
     print(label_str)
 
-    # Beit + huBert
-    model = fun.VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
-            "microsoft/beit-base-patch16-384", "SZTAKI-HLT/hubert-base-cc")
-
+    # print('step1')
+    model = fun.VisionEncoderDecoderModel.from_encoder_decoder_pretrained("google/vit-base-patch16-384", "NYTK/text-generation-poem-petofi-gpt2-small-hungarian")
+    print(model.decoder.__dict__)
+    # set decoder config to causal lm
+    model.config.decoder.is_decoder = True
+    model.config.decoder.add_cross_attention = True
     # set special tokens used for creating the decoder_input_ids from the labels
-    model.config.decoder_start_token_id = fun.processor.tokenizer.cls_token_id
-    assert model.config.decoder_start_token_id == fun.processor.tokenizer.cls_token_id
+    model.config.decoder_start_token_id = fun.processor.tokenizer.bos_token_id #cls_token_id
     model.config.pad_token_id = fun.processor.tokenizer.pad_token_id
     # make sure vocab size is set correctly
     model.config.vocab_size = model.config.decoder.vocab_size
 
     # set beam search parameters
     model.config.eos_token_id = fun.processor.tokenizer.sep_token_id
-    model.config.max_length = 64
+    model.config.max_length = 128 #64
     model.config.early_stopping = True
     model.config.no_repeat_ngram_size = 3
     model.config.length_penalty = 2.0
@@ -47,20 +50,20 @@ def main():
     # CUDA_LAUNCH_BLOCKING=1
     training_args = fun.Seq2SeqTrainingArguments(
         dataloader_num_workers=0,
-        num_train_epochs=10,
-        learning_rate=1e-4,
+        num_train_epochs=12,
+        learning_rate=2e-5,
         predict_with_generate=True,
         evaluation_strategy="steps",
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        fp16=True,
-        output_dir=f'models/Beit_HuBert/{fun.datetime.now().strftime("%Y%m%d%H%M%S")}',
+        fp16=False,
+        output_dir=f'models/ViT_gpt2_small_hu/{fun.datetime.now().strftime("%Y%m%d%H%M%S")}',
         logging_steps=100,
         save_steps=1000,
         eval_steps=500,
     )
 
-    # instantiate trainer
+    # # instantiate trainer
     trainer = fun.Seq2SeqTrainer(
         model=model,
         tokenizer=fun.processor.feature_extractor,
@@ -71,6 +74,7 @@ def main():
         data_collator=fun.default_data_collator,
     )
     trainer.train()
+
 
 if __name__ == '__main__':
     main()
