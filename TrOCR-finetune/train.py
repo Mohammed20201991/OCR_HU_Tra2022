2359e5c7ac88cee26a3c9eaa0339ec0f4286dd83
Mohammed20201991 <mohamedabid092@gmail.com>
2022-10-27 15:31:36 +0200

    Useing ViT Transformers for Featuer Extracter (Enc)& BERT Tr as Text Generator(Dec)

--- TrOCR-finetune/train.py
+++ TrOCR-finetune/train.py
@@ -1,189 +1,81 @@
-from datetime import datetime
-import os
 import sys
-import pandas as pd
-import torch
-from PIL import Image
-from datasets import load_metric
-from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
-from transformers import TrOCRProcessor
-from transformers import VisionEncoderDecoderModel
-from transformers import default_data_collator
-import argparse
+sys.path.append('/home/ngyongyossy/mohammad/asdf/TrOCR-finetune/')
+import fun
+import unit_test
 
-parser = argparse.ArgumentParser(description="Example script for finetuning TrOCR model",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("text_path", help="Location of transcriptions (single text file)")
-parser.add_argument("images_path", help="Location of image files (folder)")
-args = parser.parse_args()
-config = vars(args)                            
+fun.os.environ['CUDA_LAUNCH_BLOCKING'] = "4"
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
-
-cer_metric = load_metric("cer")
-wer_metric = load_metric('wer')
-processor = TrOCRProcessor.from_pretrained("ycchen/TrOCR-base-ver021-v1")
-
-train_text = config['text_path']
-train_img = config['images_path']
-
-def compute_metrics(pred):
-    labels_ids = pred.label_ids
-    pred_ids = pred.predictions
-
-    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
-    labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
-    label_str = processor.batch_decode(labels_ids, skip_special_tokens=True)
-
-    cer = cer_metric.compute(predictions=pred_str, references=label_str) * 100
-    wer = wer_metric.compute(predictions=pred_str, references=label_str) * 100
-    return {"cer": cer, 'wer': wer}
-
-
-class LaiaDataset(Dataset):
-    def __init__(self, root_dir, df, processor, max_target_length=128):
-        self.root_dir = root_dir
-        self.df = df
-        self.processor = processor
-        self.max_target_length = max_target_length
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        # get file name + text
-        file_name = self.df['file_name'][idx]
-        text = self.df['text'][idx]
-        # prepare image (i.e. resize + normalize)
-        image = Image.open(self.root_dir + file_name).convert("RGB")
-        pixel_values = self.processor(image, return_tensors="pt").pixel_values
-        # add labels (input_ids) by encoding the text
-        labels = self.processor.tokenizer(text,
-                                          padding="max_length",
-                                          max_length=self.max_target_length).input_ids
-        # important: make sure that PAD tokens are ignored by the loss function
-        labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in
-                  labels]
-
-        encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
-        return encoding
-
-
-def load_dataset():
-    df = pd.read_fwf('dataset/gt_test.txt', header=None)
-    df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
-    df['file_name'] = df['file_name'].apply(lambda x: x + '.jpg')
-
-    return df
-
-
-def load_laia() -> pd.DataFrame:
-    # df = pd.read_csv('/home/levaid/PyLaia-hungarian-ex-pycharm/iam-htr/data/original/lines.txt',
-    #                 sep=' ', header=None)
-
-    df = pd.read_csv('/home/ngyongyossy/mohammad/TrOCR/training-data-ex/lines.txt',sep=' ', header=None)
-    data = []
-    print(train_text)
-    with open(train_text) as infile:
-        for line in infile:
-            file_name, _, _, _, _, _, _, _, text = line.strip().split(' ')
-            data.append((file_name, clean_text(text)))
-
-    df = pd.DataFrame(data, columns=['file_name', 'text'])
-    df.rename(columns={0: 'file_name', 8: 'text'}, inplace=True)
-    df['file_name'] = df['file_name'].apply(lambda x: x + '.jpg')
-    df = df[['file_name', 'text']]
-    return df
-
-
-def clean_text(input_text: str) -> str:
-    text = input_text.replace('+', '-')
-    text = text.replace('|', ' ')
-    return text
-
-
-def create_datasets(df: pd.DataFrame):
-    train_df, test_df = train_test_split(df, test_size=0.05, random_state=42069)
-    # we reset the indices to start from zero
-    train_df.reset_index(drop=True, inplace=True)
-    test_df.reset_index(drop=True, inplace=True)
-
-    train_dataset = LaiaDataset(
-        root_dir=train_img,
-        df=train_df,
-        processor=processor)
-    eval_dataset = LaiaDataset(
-        root_dir=train_img,
-        df=test_df,
-        processor=processor)
-
-    return train_dataset, eval_dataset
+fun.processor.tokenizer = fun.AutoTokenizer.from_pretrained("bert-base-uncased")
 
 def main():
-    df = load_laia()
+  
+    df = fun.load_laia()
     # df = load_dataset()
     print(df.head())
-    train_dataset, eval_dataset = create_datasets(df)
+    train_dataset, eval_dataset = fun.create_datasets(df)
 
     print("Number of training examples:", len(train_dataset))
     print("Number of validation examples:", len(eval_dataset))
 
     encoding = train_dataset[0]
-    for k, v in encoding.items():
+    for k,v in encoding.items():
         print(k, v.shape)
-    print('step0')
-    labels = encoding['labels']
-    labels[labels == -100] = processor.tokenizer.pad_token_id
-    label_str = processor.decode(labels, skip_special_tokens=True)
-    print(label_str)
 
-    print('step1') 
-    model = VisionEncoderDecoderModel.from_pretrained("ycchen/TrOCR-base-ver021-v1") #,"microsoft/trocr-large-handwritten"
+    labels = encoding['labels']
+    labels[labels == -100] = fun.processor.tokenizer.pad_token_id
+    label_str = fun.processor.decode(labels, skip_special_tokens=True)
+    print(label_str)
+    
+    model = fun.VisionEncoderDecoderModel.from_encoder_decoder_pretrained("google/vit-base-patch16-384","bert-base-uncased")
+    # set decoder config to causal lm
+    model.config.decoder.is_decoder = True
+    model.config.decoder.add_cross_attention = True
+
+    #model.decoder.__dict__
+
     # set special tokens used for creating the decoder_input_ids from the labels
-    model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
-    model.config.pad_token_id = processor.tokenizer.pad_token_id
+    model.config.decoder_start_token_id = fun.processor.tokenizer.cls_token_id
+    assert model.config.decoder_start_token_id == fun.processor.tokenizer.cls_token_id
+    model.config.pad_token_id = fun.processor.tokenizer.pad_token_id
     # make sure vocab size is set correctly
     model.config.vocab_size = model.config.decoder.vocab_size
 
     # set beam search parameters
-    model.config.eos_token_id = processor.tokenizer.sep_token_id
-    model.config.max_length = 64
+    model.config.eos_token_id = fun.processor.tokenizer.sep_token_id
+    model.config.max_length = 128
     model.config.early_stopping = True
     model.config.no_repeat_ngram_size = 3
     model.config.length_penalty = 2.0
     model.config.num_beams = 4
-    print(model.__dict__)
-    # Saving Our Model
-    # model.save_pretrained("./models/Which_Vision_Beast/Vit_CLM_HuTrOCR")
-    training_args = Seq2SeqTrainingArguments(
-        num_train_epochs=12,
-        learning_rate=2e-5,
+
+    # from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+
+    training_args = fun.Seq2SeqTrainingArguments(
         predict_with_generate=True,
         evaluation_strategy="steps",
-        per_device_train_batch_size=24,
-        per_device_eval_batch_size=24,
+        learning_rate=2e-5,
+        num_train_epochs=12,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
         fp16=True,
-        # Which_Vision_Beast/Vit_CLM_TrOCR
-        output_dir=f'models/TrOCR-base-ver021-aicup-ft{datetime.now().strftime("%Y%m%d%H%M%S")}',
+        output_dir="./vit_bert",
         logging_steps=100,
         save_steps=1000,
         eval_steps=500,
     )
 
     # instantiate trainer
-    trainer = Seq2SeqTrainer(
+    trainer = fun.Seq2SeqTrainer(
         model=model,
-        tokenizer=processor.feature_extractor,
+        tokenizer= fun.processor.feature_extractor,
         args=training_args,
-        compute_metrics=compute_metrics,
+        compute_metrics= fun.compute_metrics,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        data_collator=default_data_collator,
+        data_collator= fun.default_data_collator,
     )
-    trainer.train()
 
+    trainer.train()
 
 if __name__ == '__main__':
     main()
