9741ed7b13753bc71b34d75e6ef7d2dd5c43865a
Mohammed20201991 <mohamedabid092@gmail.com>
2022-10-27 16:19:56 +0200

    Vit with Distlibert

--- TrOCR-finetune/train.py
+++ TrOCR-finetune/train.py
@@ -1,13 +1,15 @@
 import fun
 import unit_test
 fun.os.environ['CUDA_VISIBLE_DEVICES'] = '5'
-fun.os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
-from transformers import DistilBertTokenizer
-
+processor = fun.TrOCRProcessor.from_pretrained("microsoft/trocr-base-printed")   #microsoft/trocr-base-handwritten
+# from byte_offset_tokenizer import ByteOffsetTokenizer
 # from transformers import AutoTokenizer
-fun.processor.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
-#AutoTokenizer.from_pretrained("distilbert-base-multilingual-cased")
+processor.tokenizer = fun.AutoTokenizer.from_pretrained("distilbert-base-cased")
+# model = AutoModel.from_pretrained("SzegedAI/charmen-electra", trust_remote_code=True)
+
+
+
 def main():
     df = fun.load_laia()
     # df = load_dataset()
@@ -29,7 +31,7 @@ def main():
     print(label_str)
 
     # print('step1')
-    model = fun.VisionEncoderDecoderModel.from_encoder_decoder_pretrained("google/vit-base-patch16-384", "bert-base-multilingual-uncased")
+    model = fun.VisionEncoderDecoderModel.from_encoder_decoder_pretrained("google/vit-base-patch16-384", "facebook/nllb-200-distilled-600M")
 
     # set special tokens used for creating the decoder_input_ids from the labels
     model.config.decoder_start_token_id = fun.processor.tokenizer.cls_token_id
@@ -45,20 +47,19 @@ def main():
     model.config.length_penalty = 2.0
     model.config.num_beams = 4
 
-    
-   
+
     training_args = fun.Seq2SeqTrainingArguments(
         dataloader_num_workers=0,
         num_train_epochs=12,
         learning_rate=2e-5,
         predict_with_generate=True,
         evaluation_strategy="steps",
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
         fp16=False,
-        output_dir=f'models/ViT_distilbert/{fun.datetime.now().strftime("%Y%m%d%H%M%S")}',
+        output_dir=f'models/ViT_distilled2/{fun.datetime.now().strftime("%Y%m%d%H%M%S")}',
         logging_steps=100,
-        save_steps=2000,
+        save_steps=1000,
         eval_steps=200,
     )
 
